b7d42d54a900acf3513b43e65eeb54cac35adaef
SpvaigPvtLtd <info@spvaig.in>
2021-06-03 12:08:39 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -1,15 +1,17 @@
-
+  
 var mysql = require('mysql')
-
+require('dotenv').config()
 
 const pool = mysql.createPool({
  
- host : 'localhost',
-   user: 'root',
-    password : '123',
+ host : 'db-mysql-blr1-05486-do-user-9022348-0.b.db.ondigitalocean.com',
+   user: 'doadmin',
+    password : 'm8wcz9akv2dbl1jy',
     database: 'e-commerce',
-    port:'3306' ,
+
+    port:'25060' ,
     multipleStatements: true
   })
 
-  module.exports = pool;  
+
+module.exports = pool;
